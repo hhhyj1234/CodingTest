ebfa40eb186b7b8a7d28ce5346f3cb1d81983d1f
hhhyj1234 <hhhyj1234@gmail.com>
2022-11-19 23:16:38 +0900

    11.19

---
+++ Programmers/lv.0/문자열 계산하기.js
@@ -0,0 +1,8 @@
+// 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
+
+/**my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때,
+ * 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요. */
+
+function solution(my_string) {
+  return eval(my_string);
+}
