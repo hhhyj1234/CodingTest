c6f39ad9a8ab4b24c021dc0686c4970f1c0aab14
hhhyj1234 <hhhyj1234@gmail.com>
2022-11-30 21:51:22 +0900

    11.30

--- Programmers/lv.0/로그인 성공?.js
+++ Programmers/lv.0/로그인 성공?.js
@@ -1,10 +1,29 @@
 // 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
+// 다른 사람 코드
+function solution(id_pw, db) {
+  let user = db.find(([u]) => u === id_pw[0]);
+
+  if (user) {
+    return user[1] === id_pw[1] ? 'login' : 'wrong pw';
+  } else {
+    return 'fail';
+  }
+}
 
 function solution(id_pw, db) {
   const [id, pw] = id_pw;
   const findId = db.filter((v) => v[0] === id).flat();
   return findId.length === 0 ? 'fail' : findId[1] === pw ? 'login' : 'wrong pw';
 }
+/** runtime
+테스트 1 〉	통과 (0.09ms, 33.4MB)
+테스트 2 〉	통과 (0.06ms, 33.4MB)
+테스트 3 〉	통과 (0.05ms, 33.4MB)
+테스트 4 〉	통과 (0.06ms, 33.5MB)
+테스트 5 〉	통과 (0.08ms, 33.4MB)
+테스트 6 〉	통과 (0.06ms, 33.4MB)
+테스트 7 〉	통과 (0.09ms, 33.4MB)
+테스트 8 〉	통과 (0.05ms, 33.5MB) */
 
 // function solution(id_pw, db) {
 //   const dbs = db.flat();
