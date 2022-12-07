3047378855f0907654453b270a515879f307359e
hhhyj1234 <hhhyj1234@gmail.com>
2022-12-07 22:20:34 +0900

    Update 삼각형의 완성조건.js

--- Programmers/lv.0/삼각형의 완성조건.js
+++ Programmers/lv.0/삼각형의 완성조건.js
@@ -3,9 +3,14 @@
 삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요. */
 
 function solution(sides) {
+  return Math.min(...sides) * 2 - 1;
+}
+
+function solution(sides) {
   const [a, b] = sides.sort((a, b) => b - a); // a>b
   return 2 * b - 1;
 }
+
 function solution(sides) {
   return sides.sort((a, b) => a - b)[0] * 2 - 1;
 }
